d266983475fa55f449f793fe58ff08e955adc1bc
Shashank Agrawal <154488267+LordCenk@users.noreply.github.com>
2026-06-06 23:58:36 +0530

    Add recursion methods for printing numbers
    Implemented recursive methods to print numbers in increasing and decreasing order.

---
+++ Recusrsion Basics(1)/PrintNumbersInIncreasingOrder.java
@@ -0,0 +1,29 @@
+public class Recursion {
+    public static void printDec(int n){
+        if(n == 1){
+            System.out.println(n);
+            return;
+        }
+        System.out.print(n+" ");
+        printDec(n-1);
+        
+    }
+    public static void printInc(int n){
+        if(n == 1){
+            System.out.print(n+" ");
+            return;
+        }
+         
+        printInc(n-1);
+        System.out.print(n+" ");
+
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        printInc(n);
+    }
+    
+}
+
+// In java whenever a function call another function then a call stack is formed
+o/p- 1 2 3 4 5 6 7 8 9 10 
